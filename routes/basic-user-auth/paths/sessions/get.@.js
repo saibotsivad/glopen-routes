24936f3a245a11f24ead33cfcfcc140afa4b7c08
Tobias Davis <tobias@davistobias.com>
2021-10-26 17:00:13 -0500

    feature: even more things

--- routes/basic-user-auth/paths/sessions/get.@.js
+++ routes/basic-user-auth/paths/sessions/get.@.js
@@ -13,14 +13,26 @@ export const parameters = [
 		description: 'Filter the session list by some parameters, e.g. `filter[attributes.status]=ok`.',
 		name: 'filter',
 		in: 'query',
+		// https://stackoverflow.com/questions/57808396/how-do-i-express-json-api-sparse-fieldsets-with-openapi-3-0
 		style: 'deepObject',
 		allowReserved: true,
 		schema: {
 			type: 'object',
-			additionalProperties: { type: 'string' },
+			additionalProperties: {
+				type: 'string'
+			},
 			example: {
 				'attributes.status': 'ok'
 			}
+		}
+	},
+	{
+		description: 'Sort the session list by some parameters, e.g. `sort=-meta.created`.',
+		name: 'sort',
+		in: 'query',
+		allowReserved: true,
+		schema: {
+			type: 'string'
 		}
 	}
 ]
---
+++ routes/basic-user-auth/paths/user/post.@.js
@@ -0,0 +1,64 @@
+import { basicUserAuth } from '../../tags.@.js'
+
+export const summary = 'Create User'
+
+export const description = `
+Create a user by providing an email address. If the user already exists, return an error.
+`
+
+export const tags = [ basicUserAuth.name ]
+
+export const requestBody = {
+	description: 'Create a new user.',
+	content: {
+		'application/json': {
+			schema: {
+				$ref: '#/components/schemas/user'
+			}
+		}
+	}
+}
+
+export const responses = {
+	201: {
+		description: 'The user and session were created, and the user object and (optionally) session are returned.',
+		headers: {
+			'Set-Cookie': {
+				description: 'The session cookie is set.'
+			}
+		},
+		content: {
+			'application/json': {
+				schema: {
+					type: 'object',
+					properties: {
+						data: {
+							$ref: '#/components/schemas/user'
+						},
+						included: {
+							type: 'array',
+							items: {
+								$ref: '#/components/schemas/session'
+							}
+						}
+					}
+					}
+				}
+			}
+	},
+	default: {
+		$ref: '#/components/responses/error'
+	}
+}
+
+export default async request => {
+	const { user, cookie } = await request.controller.user.create(request)
+	const response = {
+		status: 201,
+		body: { data: user }
+	}
+	if (cookie) {
+		response.headers = { 'Set-Cookie': cookie }
+	}
+	return response
+}
