c791ff4fbfbba081a5200f2c8c385381b352fe9b
Tobias Davis <tobias@davistobias.com>
2021-11-11 16:25:03 -0600

    fix: error body is plural

--- definition/_shared/openapi/components/schemas/errorBody.@.js
+++ definition/_shared/openapi/components/schemas/errorBody.@.js
@@ -2,7 +2,7 @@ export default {
 	description: 'The response body when the only thing returned is one or more errors.',
 	type: 'object',
 	properties: {
-		error: {
+		errors: {
 			type: 'array',
 			items: {
 				$ref: '#/components/schemas/error',
