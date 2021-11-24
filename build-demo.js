ae4afa1221387cee25e73df1fe4161d2233420e6
Tobias Davis <tobias@davistobias.com>
2021-11-24 12:02:16 -0600

    feature: render docs with redocly as well, for 3.1.0 support

--- build-demo.js
+++ build-demo.js
@@ -7,13 +7,29 @@ import { dump } from 'js-yaml'
 import { compile } from 'ajv-openapi-compile'
 
 const NAME = process.argv.slice(2)
-const HTML_FILE = './' + join('demo', `${NAME}.html`)
+const SWAGGER_HTML_FILE = './' + join('demo', `${NAME}-swagger.html`)
+const REDOC_HTML_FILE = './' + join('demo', `${NAME}-redoc.html`)
 const JS_FILE = './' + join('demo', `${NAME}.js`)
 const JSON_FILE = './' + join('demo', `${NAME}.json`)
 const YAML_FILE = './' + join('demo', `${NAME}.yaml`)
 const AJV_FILE = './' + join('demo', `${NAME}-ajv.js`)
 
 const lines = []
+
+const htmlTemplate = ({ name }) => `<!DOCTYPE html>
+<html lang="en-US">
+<head>
+	<title>Glopen Routes: ${name}</title>
+	<meta name="viewport" content="width=device-width, initial-scale=1" />
+	<meta name="description" content="Reference documentation and OpenAPI specification for the '${name}' portion of the Glopen Routes API." />
+	<meta charset="utf-8" />
+</head>
+<body>
+	<redoc spec-url="/all.yaml" hide-hostname="true" lazy-rendering></redoc>
+	<script src="https://cdn.jsdelivr.net/npm/redoc@2.0.0-alpha.17/bundles/redoc.standalone.js"></script>
+</body>
+</html>
+`
 
 const writeGeneratedFiles = () => writeFile(JS_FILE, lines.join('\n'), 'utf8')
 	.then(() => import(JS_FILE))
@@ -24,12 +40,13 @@ const writeGeneratedFiles = () => writeFile(JS_FILE, lines.join('\n'), 'utf8')
 	]))
 	.then(() => readFile(join('demo', 'swagger', 'index.html'), 'utf8'))
 	.then(html => writeFile(
-		HTML_FILE,
+		SWAGGER_HTML_FILE,
 		html
 			.replace(/\.\//g, './swagger/')
 			.replace('dom_id:', `url: "./${NAME}.yaml",\n      dom_id:`),
 		'utf8',
 	))
+	.then(() => writeFile(REDOC_HTML_FILE, htmlTemplate({ name: NAME }), 'utf8'))
 
 process.stdin.setEncoding('utf8')
 process.stdin.on('readable', () => {
